7d25b7935dc3af17b6c2ba94bb2f446689c008b3
Jimmy Wen <JimmyWen@Jimmys-MacBook.local>
2020-04-09 04:14:29 +0800

    moment.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+const monthCoundown = () => {
+  const dayOfMonth = 10;
+  const hour = 17;
+  const yearNow = moment().year();
+  const monthNow = moment().month();
+  const targetTime = moment([0, 0, dayOfMonth]).year(yearNow).month(monthNow + 1).hour(hour).format("YYYY-MM-DDTHH:mm");
+  console.log(targetTime);
+}
+
+
 const closeSetup = () => {
   document.querySelector('.ios-setup').style.display = 'none';
 }
